906104cab0e422aab3e2f89974884c6ea2dcf378
tastaslim <taslimarif600@gmail.com>
2020-07-19 12:29:25 +0530

    Settings of all requirements for directory structure

--- models/user.js
+++ models/user.js
@@ -1,25 +1,23 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema(
-	{
-		email: {
-			type: String,
-			required: true,
-			unique: true,
-		},
-		password: {
-			type: String,
-			required: true,
-		},
-		name: {
-			type: String,
-			required: true,
-		},
-	},
-	{
-		timestamps: true,
-	},
-);
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
 
 const User = mongoose.model('User', userSchema);
 
